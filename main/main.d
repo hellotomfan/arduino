d8998f4384f3e8cd7786c31cd03353e7a081c4cf
tom <hellotomfan@gmail.com>
2014-02-28 16:34:06 +0800

    remove *.d file

--- main/main.d
+++
@@ -1,4 +0,0 @@
-main: main.cpp ../libu8g/U8glib.h ../libarduino/cores/arduino/Print.h \
- ../libarduino/cores/arduino/WString.h \
- ../libarduino/cores/arduino/Printable.h \
- ../libarduino/cores/arduino/new.h ../libu8g/utility/u8g.h

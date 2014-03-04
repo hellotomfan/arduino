7d2ad7a1ab3b27e59618fc1c30a71dad88ad909b
tom <hellotomfan@gmail.com>
2014-03-04 17:59:24 +0800

    test

--- main/main.cpp
+++ main/main.cpp
@@ -4,11 +4,10 @@
 U8GLIB_NHD_C12864 u8g(13, 11, 10, 9, 8);    // SPI Com: SCK = 13, MOSI = 11, CS = 10, CD = 9, RST = 8
  
 void draw(void) {
-  static uint8_t i = 0;
   // graphic commands to redraw the complete screen should be placed here 
   u8g.setFont(u8g_font_unifont);
   //u8g.setFont(u8g_font_osb21);
-  u8g.drawStr( 20, 35, "hello world");
+  u8g.drawStr(20, 35, "hello world");
 }
  
 void setup(void) {

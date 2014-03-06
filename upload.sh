b25831369b40960023f1e22537bbd5705af867c1
tom <hellotomfan@gmail.com>
2014-03-06 17:33:55 +0800

    update

--- upload.sh
+++ upload.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-avr-objcopy -O ihex main/main main.hex
-avrdude -V -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:main.hex:i
-rm main.hex
+
+MODEL=atmega328p
+DEVICE=/dev/ttyACM0
+RATE=115200
+
+if [[ "$1" != "" ]]; then
+
+	if test -e $1/$1; then
+		avr-objcopy -O ihex $1/$1 $1.hex
+		avrdude -V -p$MODEL -carduino -P$DEVICE -b$RATE -D -Uflash:w:$1.hex:i
+		rm $1.hex
+	else
+		echo "$1 not found!"
+	fi
+else
+	echo "$0 <upload_file>"
+fi
